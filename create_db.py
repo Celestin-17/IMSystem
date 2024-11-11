646ad1c61447d518dea59e4fd77caab8f79a949b
Celestin <celestinpd@icloud.com>
2024-11-11 15:07:01 +0200

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -11,7 +11,7 @@ queries = (
     CREATE TABLE IF NOT EXISTS employees (
         emp_id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
-        password VARCHAR(255) NOT NULl, 
+        password VARCHAR(255) NOT NULL,
         email VARCHAR(255) UNIQUE,
         gender VARCHAR(40),
         birthday DATE,
